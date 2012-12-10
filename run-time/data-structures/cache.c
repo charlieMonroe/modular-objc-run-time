87d40453c09437c08e93be8f646d0ac5fba6c2a7
charlieMonroe <charlie@charliemonroe.net>
2012-12-10 16:48:47 +0100

    Making the cache structure read-lock-free (locks only on write).

--- run-time/data-structures/cache.c
+++ run-time/data-structures/cache.c
@@ -10,52 +10,26 @@
 #include "method-private.h"
 #include "os.h"
 
-typedef union {
-	Method one;
-	Method *many;
-} one_or_many;
-
 typedef struct _objc_cache_bucket_struct {
-	
-	unsigned int count;
-	one_or_many elements;
+	struct _objc_cache_bucket_struct *next;
+	Method method;
 } _objc_cache_bucket;
 
 typedef struct _objc_cache_str {
 	objc_rw_lock lock;
-	unsigned int entry_count;
 	unsigned int bucket_count;
-	
-	_objc_cache_bucket *buckets; // Lazily allocated!
+	_objc_cache_bucket **buckets; // Lazily allocated!
 } *_objc_cache;
 
 
 /**
  * Returns a pointer to the bucket for that key.
  */
-static inline _objc_cache_bucket *_objc_cache_bucket_for_selector(_objc_cache table, const SEL selector){
+static inline unsigned int _objc_cache_bucket_index_for_selector(_objc_cache table, const SEL selector){
 	if (table->buckets == NULL){
 		table->buckets = objc_zero_alloc(sizeof(_objc_cache_bucket) * table->bucket_count);
 	}
-	return &(table->buckets[(unsigned int)selector & (table->bucket_count - 1)]);
-}
-
-static inline void objc_cache_clear(_objc_cache table){
-	int i;
-	for (i = 0; i < table->bucket_count; ++i){
-		_objc_cache_bucket bucket = table->buckets[i];
-		if (bucket.count == 0){
-			continue;
-		}
-		
-		if (bucket.count > 1){
-			objc_dealloc(bucket.elements.many);
-		}
-	}
-	
-	// Free buckets
-	objc_dealloc(table->buckets);
-	table->buckets = NULL;
+	return (unsigned int)selector & (table->bucket_count - 1);
 }
 
 static inline unsigned int log2u(unsigned int x) { return (x<2) ? 0 : log2u (x>>1)+1; };
@@ -72,106 +46,67 @@ static inline void _objc_cache_initialize_buckets(_objc_cache table){
 
 static inline _objc_cache objc_cache_create_internal(){
 	_objc_cache cache = (_objc_cache)(objc_alloc(sizeof(struct _objc_cache_str)));
-	cache->entry_count = 0;
 	cache->buckets = NULL;
 	cache->lock = objc_rw_lock_create();
 	cache->bucket_count = GOOD_CAPACITY(64); // A usual class uses ~64 methods
 	return cache;
 }
-
-static inline void objc_cache_destroy_internal(_objc_cache cache){
-	objc_cache_clear(cache);
-	if (cache->lock != NULL){
-		objc_rw_lock_destroy(cache->lock);
+static inline BOOL _cache_contains_method_in_bucket(_objc_cache_bucket *bucket, Method m){
+	while (bucket != NULL) {
+		if (bucket->method == m){
+			// Already there!
+			return YES;
+		}
+		
+		bucket = bucket->next;
 	}
-	objc_dealloc(cache);
+	return NO;
 }
+
 static inline void objc_cache_insert_method(_objc_cache cache, Method m){
 	if (cache->buckets == NULL){
 		_objc_cache_initialize_buckets(cache);
 	}
 	
-	_objc_cache_bucket *bucket = _objc_cache_bucket_for_selector(cache, m->selector);
-	unsigned int bucket_size = bucket->count;
-	
-	if (bucket_size == 0){
-		// First item in the bucket
-		++bucket->count;
-		++cache->entry_count;
-		bucket->elements.one = m;
+	unsigned int bucket_index = _objc_cache_bucket_index_for_selector(cache, m->selector);
+	if (_cache_contains_method_in_bucket(cache->buckets[bucket_index], m)){
+		// Already contains
 		return;
 	}
 	
-	if (bucket_size == 1){
-		// Are the entries equal?
-		if (bucket->elements.one == m){
-			// YES -> we're done
-			return;
-		}
-		
-		// No, need to become elements.many
-		Method *elements = (Method*)objc_zero_alloc(2 * sizeof(void*));
-		elements[1] = bucket->elements.one;
-		elements[0] = m;
-		bucket->elements.many = elements;
-		
-		++bucket->count;
-		++cache->entry_count;
+	// Prepare the bucket before locking in order to
+	// keep the structure locked as little as possible
+	_objc_cache_bucket *bucket = (_objc_cache_bucket*)objc_alloc(sizeof(struct _objc_cache_bucket_struct));
+	bucket->method = m;
+	
+	// Lock the structure for insert
+	objc_rw_lock_wlock(cache->lock);
+	
+	// Someone might have inserted it between searching and locking
+	if (_cache_contains_method_in_bucket(cache->buckets[bucket_index], m)){
+		objc_rw_lock_unlock(cache->lock);
+		objc_dealloc(bucket);
 		return;
 	}
 	
-	// There are more items, see if any is equal:
-	Method *elements = bucket->elements.many;
-	unsigned int o;
-	for (o = 0; o < bucket_size; ++o){
-		if (elements[o] == m){
-			// Yes, return
-			return;
-		}
-	}
+	bucket->next = cache->buckets[bucket_index];
+	cache->buckets[bucket_index] = bucket;
 	
-	// No luck. Reallocate the list.
-	bucket->elements.many = (Method*)objc_realloc(bucket->elements.many, bucket->count + 1);
-	bucket->elements.many[bucket->count] = m;
-	bucket->count++;
-	cache->entry_count++;
-	
+	objc_rw_lock_unlock(cache->lock);
 }
 
-static Method objc_cache_fetch_method(_objc_cache table, SEL key) {
-	_objc_cache_bucket *bucket = _objc_cache_bucket_for_selector(table, key);
-	if (bucket->count == 0){
-		return NULL;
-	}
-	
-	if (bucket->count == 1){
-		if (bucket->elements.one->selector == key){
-			return bucket->elements.one;
+static Method objc_cache_fetch_method(_objc_cache cache, SEL key) {
+	unsigned int bucket_index = _objc_cache_bucket_index_for_selector(cache, key);
+	_objc_cache_bucket *bucket = cache->buckets[bucket_index];
+	while (bucket != NULL) {
+		if (bucket->method->selector == key){
+			return bucket->method;
 		}
-	}
-	
-	unsigned int i;
-	for (i = 0; i < bucket->count; ++i){
-		if (bucket->elements.many[i]->selector == key){
-			// Found it
-			return bucket->elements.many[i];
-		}
+		bucket = bucket->next;
 	}
 	
 	return NULL;
 }
-
-static inline void objc_cache_rlock(_objc_cache cache){
-	objc_rw_lock_rlock(cache->lock);
-}
-static inline void objc_cache_wlock(_objc_cache cache){
-	objc_rw_lock_wlock(cache->lock);
-}
-static inline void objc_cache_unlock(_objc_cache cache){
-	objc_rw_lock_unlock(cache->lock);
-}
-
-
 
 
 objc_cache objc_cache_create(void){
